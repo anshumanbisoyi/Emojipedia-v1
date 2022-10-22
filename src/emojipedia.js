1da230e697295175386dc0e515f8d9a1c2c244f2
Anshuman Bisoyi <71143398+anshumanbisoyi@users.noreply.github.com>
2022-10-22 21:56:54 +0530

    Update test files

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -20,5 +20,12 @@ const emojipedia = [
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
   }
+  {
+    id: 4,
+    emoji: "😭",
+    name: "Loudly crying face",
+    meaning:
+      "This is sad! A crying face. The face is crying mercilessly. The emoji version of “sed“."
+  }
 ];
 export default emojipedia;
